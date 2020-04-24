c7ff6822dd248d20cee3a092480772072dccb3d8
Harrison <hmseaborn@gmail.com>
2020-04-23 17:59:07 -0700

    up with nav bar and a basic home layout

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+
+const Home = () => {
+	return (
+		<div className='home-container'>
+      <div className='home-left-cont'>
+        <h1 className='home-h1'>Hi I'm Harrison Michael Seaborn</h1>
+      </div>
+      <div className='home-right-cont'>
+        <h4 className='home-h4'>I am a Full Stack Web Developer, from Spokane, WA</h4>
+      </div>
+		</div>
+	);
+};
+
+export default Home;
