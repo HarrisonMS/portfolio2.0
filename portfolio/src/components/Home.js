78e43a228ae37bd0da76e1f36530d455fff18caa
Harrison <hmseaborn@gmail.com>
2020-05-06 08:49:51 -0700

    idk

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,15 +1,16 @@
 
 import React from 'react';
-
+import Me from '../img/me.jpg'
 
 const Home = () => {
 	return (
 		<div className='home-container'>
       <div className='home-left-cont'>
-        <h1 className='home-h1'>Hi I'm Harrison Michael Seaborn</h1>
+        <h1 className='home-h1'>I'm Harrison Michael Seaborn</h1>
       </div>
       <div className='home-right-cont'>
-        <h4 className='home-h4'>I am a Full Stack Web Developer, from Spokane, WA</h4>
+      <img className='me-img' src={Me} alt='young harrison' />
+        <h4 className='home-h4'>I'm a Full Stack Web Developer, from Spokane, WA</h4>
       </div>
 		</div>
 	);
