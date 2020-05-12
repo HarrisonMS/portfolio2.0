3926510a5694d62a18fcc4fd0bf84c2e771976eb
Harrison <hmseaborn@gmail.com>
2020-05-12 15:22:42 -0700

    Update Header.js

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,30 +1,28 @@
-
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
-	return (
-		<div className='headerContainer'>
-			<Link className='headerTitle' to='/'>
-				<p>Harrison Micheal Seaborn</p>
-			</Link>
-			<nav className='nav-link-cont'>
-        <NavLink className='nav-link' to='/'>
-				  Home
-				</NavLink>
-				<NavLink className='nav-link' to='/'>
-					Projects
-				</NavLink>
-				<NavLink className='nav-link' to='/resume'>
-					Resume
-				</NavLink>
-				<NavLink className='nav-link' to='/contact'>
-					Contact
-				</NavLink>
-			</nav>
-
-		</div>
-	);
+  return (
+    <div className="headerContainer">
+      <Link className="headerTitle" to="/">
+        <p>Harrison Micheal Seaborn</p>
+      </Link>
+      <nav className="nav-link-cont">
+        <NavLink className="nav-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="nav-link" to="/projects">
+          Projects
+        </NavLink>
+        <NavLink className="nav-link" to="/resume">
+          Resume
+        </NavLink>
+        <NavLink className="nav-link" to="/contact">
+          Contact
+        </NavLink>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
