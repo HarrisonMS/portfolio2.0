191372b8ffe96b02088323863422b4b1053d39d3
Harrison <hmseaborn@gmail.com>
2020-05-19 08:47:23 -0700

    rid of dang node modules
    idk who did that npm i in the root folder lol twas i

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio2.0
-this is the beginging of the end
---
+++ portfolio/src/utilities/screenSizes.js
@@ -0,0 +1,7 @@
+export const mobileSmall = "@media screen and (max-width: 320 px)";
+export const mobileMedium = "@media screen and (max-width: 375 px)";
+export const mobileLarge = "@media screen and (max-width: 425 px)";
+export const tablet = "@media screen and (max-width: 768 px)";
+export const laptop = "@media screen and (max-width: 1024 px)";
+export const laptopLarge = "@media screen and (max-width: 1440 px)";
+export const fourk = "@media screen and (max-width: 2560 px)";
