442252b1072d07bc04e9728a1bac50c88174860e
Harrison <hmseaborn@gmail.com>
2020-05-19 08:47:32 -0700

    Update Home.js

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,19 +1,20 @@
-
-import React from 'react';
-import Me from '../img/me.jpg'
+import React from "react";
+import Me from "../img/me.jpg";
 
 const Home = () => {
-	return (
-		<div className='home-container'>
-      <div className='home-left-cont'>
-        <h1 className='home-h1'>I'm Harrison Michael Seaborn</h1>
+  return (
+    <div className="home-container">
+      <div className="home-left-cont">
+        <h1 className="home-h1">Harrison Michael Seaborn</h1>
       </div>
-      <div className='home-right-cont'>
-      <img className='me-img' src={Me} alt='young harrison' />
-        <h4 className='home-h4'>I'm a Full Stack Web Developer, from Spokane, WA</h4>
+      <div className="home-right-cont">
+        <img className="me-img" src={Me} alt="young harrison" />
+        <h4 className="home-h4">
+          I'm a Full Stack Web Developer, from Spokane, WA
+        </h4>
       </div>
-		</div>
-	);
+    </div>
+  );
 };
 
 export default Home;
