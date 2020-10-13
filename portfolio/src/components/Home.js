b20cf6eeb9c507883eea1d07a7281831cdd64b81
Harrison Seaborn <54726103+HarrisonMS@users.noreply.github.com>
2020-10-13 13:46:27 -0700

    shrug

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Me from "../img/me.jpg";
 
 const Home = () => {
+  const [quote, setquote] = useState([]);
+  const [loading, setloading] = useState(false);
+  async function getQuote() {
+    const proxyUrl = "https://whispering-tor-04671.herokuapp.com/";
+    const apiUrl =
+      "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    try {
+      const response = await fetch(proxyUrl + apiUrl);
+      const data = await response.json();
+      console.log("data", data);
+      setquote(data);
+    } catch (error) {
+      getQuote();
+    }
+  }
+  useEffect(() => {
+    getQuote();
+  }, [quote]);
   return (
     <div className="home-container">
       <div className="home-left-cont">
-        <h1 className="home-h1">Harrison Michael Seaborn</h1>
+        <h1 className="home-h1">{quote.quoteText}</h1>
       </div>
       <div className="home-right-cont">
         <img className="me-img" src={Me} alt="young harrison" />
